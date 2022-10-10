72f56bdcb59fc5b92df94c9a3cb4edd58c8b009f
pon <pon@ponde-MacBook-Pro.local>
2022-10-10 16:12:15 +0800

    ver4

--- public/js/square.js
+++ public/js/square.js
@@ -3,5 +3,6 @@ let input = document.querySelector(".countInput")
 
 btn.addEventListener("click", ()=>{
   input.value == "" ? alert("請輸入數字") : Number.isInteger(parseInt(input.value)) == false
-  ? alert("無法解讀") : parseInt(input.value) <= 0 ? alert("請輸入大於 0 的整數") : null
+  ? alert("無法解讀") : parseInt(input.value) <= 0 
+  ? alert("請輸入大於 0 的整數") : null
 })
