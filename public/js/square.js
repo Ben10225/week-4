e2f045b4476df003f38f78d407858c9176d685d5
pon <pon@ponde-MacBook-Pro.local>
2022-10-11 11:03:57 +0800

    modify dynamic route

--- public/js/square.js
+++ public/js/square.js
@@ -5,4 +5,7 @@ btn.addEventListener("click", ()=>{
   input.value == "" ? alert("請輸入數字") : Number.isInteger(parseInt(input.value)) == false
   ? alert("無法解讀") : parseInt(input.value) <= 0 
   ? alert("請輸入大於 0 的整數") : null
+
+  input.value == "" ? url = '/square/a' : url = '/square/' + `${input.value}`
+  window.location.href = url;
 })
