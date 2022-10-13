d2c202f4fa796e1093d564afc449910cc3690da1
pon <pon@ponde-MacBook-Pro.local>
2022-10-13 17:27:19 +0800

    cookie

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 
-from flask import Flask, render_template, redirect, request, session, url_for
+from urllib import response
+from flask import Flask, render_template, redirect, session, request, make_response, Response
 import re
 
 app = Flask(__name__, static_folder="public", static_url_path="/")
-app.debug = True
+# app.debug = True
 
 app.secret_key="anytxt"
 
@@ -13,8 +14,14 @@ def index():
 
 @app.route("/member")
 def member():
-  if session["account"] == False:
+  # if session["account"] == False:
+  #   return redirect("/")
+
+  # cookie
+  account = request.cookies.get("account")
+  if not account :
     return redirect("/")
+
   return render_template("member.html")
 
 @app.route("/signin", methods=["post"])
@@ -22,8 +29,13 @@ def sign():
   account = request.form["account"]
   password = request.form["password"]
   if account == "test" and password == "test":
-    session["account"] = account
-    return redirect("/member")
+    # session["account"] = account
+
+    # cookie
+    resp = make_response(redirect("/member"))
+    resp.set_cookie('account', 'flask', 7200)
+    return resp
+
   else:
     if account == "" or password == "":
       return redirect("/error?message=請輸入帳號、密碼")
@@ -36,8 +48,12 @@ def fail():
 
 @app.route("/signout")
 def signout():
-  session['account'] = False
-  return redirect("/")
+  # session['account'] = False
+
+  # cookie
+  resp = make_response(redirect("/"))
+  resp.set_cookie('account', '', 0)
+  return resp
 
 @app.route("/square/<id>")
 def sqDynamic(id):
@@ -49,6 +65,7 @@ def sqDynamic(id):
     result = num**2
     print(result)
     return render_template("square.html", result=result)
+
 
 # @app.route("/square", methods=["post"])
 # def square():
