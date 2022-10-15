1cff186c6a999c709d0b762cd52d7aad15b99fe3
pon <pon@ponde-MacBook-Pro.local>
2022-10-15 15:54:10 +0800

    crypt

--- app.py
+++ app.py
@@ -1,11 +1,30 @@
 
-from flask import Flask, render_template, redirect, session, request, make_response, Response
+from flask import Flask, render_template, redirect, url_for, session, request, make_response, Response
+from cryptography.fernet import Fernet
 import re
 
 app = Flask(__name__, static_folder="public", static_url_path="/")
-# app.debug = True
+app.debug = True
 
 app.secret_key="anytxt"
+
+def generate_key():
+  return Fernet.generate_key()
+
+def encrypt(key, pwd):
+  cipher_suite = Fernet(key)
+  bpwd = bytes(pwd, 'utf-8')
+  ciphered_text = cipher_suite.encrypt(bpwd)
+  return ciphered_text
+
+def decrypt(key, enc_pwd):
+  cipher_suite = Fernet(key)
+  uncipher_text = (cipher_suite.decrypt(enc_pwd))
+  return bytes(uncipher_text).decode("utf-8")
+
+key = generate_key()
+encryptStr = encrypt(key, "cryptographyPWD")
+encryptVal = encrypt(key, "flask")
 
 @app.route("/")
 def index():
@@ -17,11 +36,17 @@ def member():
   #   # return redirect("/")
 
   # cookie
-  account = request.cookies.get("account")
-  if not account :
-    return redirect("/")
+  # account = request.cookies.get("account")
+  # if not account :
+  #   return redirect("/")
+  # return render_template("member.html")
 
-  return render_template("member.html")
+  # cryptography
+  account = request.cookies.get(bytes(encryptStr).decode("utf-8")[:-2])
+  if not account:
+    return redirect("/")
+  if account[2:] == bytes(encryptVal).decode("utf-8"):
+    return render_template("member.html")
 
 @app.route("/signin", methods=["post"])
 def sign():
@@ -32,8 +57,14 @@ def sign():
     # return redirect("/member")
 
     # cookie
+    # resp = make_response(redirect("/member"))
+    # resp.set_cookie('account', 'flask', 7200)
+    # return resp
+
+    # cryptography
     resp = make_response(redirect("/member"))
-    resp.set_cookie('account', 'flask', 7200)
+    resp.set_cookie(encryptStr, encryptVal, 7200)
+    print(bytes(encryptStr).decode("utf-8"))
     return resp
 
   else:
@@ -52,8 +83,13 @@ def signout():
   # return redirect("/")
 
   # cookie
+  # resp = make_response(redirect("/"))
+  # resp.set_cookie('account', '', 0)
+  # return resp
+
+  # cryptography
   resp = make_response(redirect("/"))
-  resp.set_cookie('account', '', 0)
+  resp.set_cookie(encryptStr, '', 0)
   return resp
 
 
@@ -67,6 +103,7 @@ def sqDynamic(id):
     result = num**2
     print(result)
     return render_template("square.html", result=result)
+
 
 
 app.run(port=3000)
